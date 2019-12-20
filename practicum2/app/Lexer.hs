ec4846727aac1971bce738f585102010fb01e3e9
StefanvdSman <sj.vandersman@gmail.com>
2019-12-20 11:13:08 +0100

    Add few things to lexer

--- practicum2/app/Lexer.hs
+++ practicum2/app/Lexer.hs
@@ -9,8 +9,25 @@ $alpha = [a-zA-Z]		-- alphabetic characters
 
 tokens :-
 
-  ->\s*       { \s -> Arrow }
-  \.\s*       { \s -> Dot }
+  ->\s*         { \s -> Arrow }
+  \.\s*         { \s -> Dot }
+  ,\s*          { \s -> Comma }
+  go\s*         { \s -> Go }
+  take\s*       { \s -> Take }
+  mark\s*       { \s -> Mark }
+  nothing\s*    { \s -> Nothing }
+  turn\s*       { \s -> Turn }
+  case\s*       { \s -> Case }
+  of\s*         { \s -> Of }
+  end\s*        { \s -> End}
+  left\s*       { \s -> Left}
+  right\s*      { \s -> Right}
+  front\s*      { \s -> Front}
+  ;\s*  { \s -> Semicolon}
+  empty\s*      { \s -> Empty}
+  Lambda\s*     { \s -> Lambda}  
+  Boundary\s*   { \s -> Boundary}
+  --.*\n        { \s -> Debris} 
 
 {
 -- Each action has type :: String -> Token
@@ -20,9 +37,9 @@ data Token = Arrow|Dot|Comma|Go|Take|Mark|Nothing
   |Turn|Case|Of|End
   |Left|Right|Front|Semicolon
   |Empty|Lambda|Debris|Asteroid|Boundary|Underscore
-  |[Ident]
+  |Idents
 	deriving (Eq,Show)
-
+data Idents = [Ident] 
 data Ident = Letter|Digit|+|-
 main = do
   s <- getContents
