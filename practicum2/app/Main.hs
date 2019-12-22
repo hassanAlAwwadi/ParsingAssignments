6cadab8a3f27369b663f4b6edd485acc0f725f3c
hassanAlAwwadi <hassan.awwadi@gmail.com>
2019-12-22 14:43:42 +0100

    Update Main.hs

--- practicum2/app/Main.hs
+++ practicum2/app/Main.hs
@@ -1,6 +1,15 @@
+-- By Hassan Al-Awwadi en Stephan (Stefan) van der Sman
 module Main where
 
 import Lib
+import Arrow
+import System.Environment
 
+-- 11
 main :: IO ()
-main = someFunc
+main = do 
+    print "By Hassan Al-Awwadi en Stephan (Stefan) van der Sman"
+    args <- getArgs 
+    case args of 
+        (env:space:_) -> readInteractive env space
+        _             -> print "please provide Arrow file as first argument and Space file as second argument"
