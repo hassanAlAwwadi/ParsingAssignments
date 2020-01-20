7c91507ba2a4c1249c341084aa359b6a7ceff930
StefanvdSman <sj.vandersman@gmail.com>
2020-01-20 22:19:10 +0100

    left n right associeativety lukt niet helemaal

--- CsharpPracticum3/src/CSharpCode.hs
+++ CsharpPracticum3/src/CSharpCode.hs
@@ -68,14 +68,15 @@ fExprVar (LowerId x) va = let loc = 37 in case va of
 fExprOp :: Token -> (ValueOrAddress -> Code) -> (ValueOrAddress -> Code) -> ValueOrAddress -> Code
 fExprOp (Operator op)  e1 e2 va |op=="*"||op=="/"||op=="%"= f
                                 |op=="+"||op=="-"= f
-                                |op=="<"||op==">"||op=="<="||op==">="= f
-                                |op=="=="||op=="!="= f
+                                |op=="<"||op==">"||op=="<="||op==">="= f2
+                                |op=="=="||op=="!="= f2
                                 |op=="^"= f
                                 |op=="&&"= f
                                 |op=="||"= f
                                 |op=="=" =  e2 Value ++ [LDS 0] ++ e1 Address ++ [STA 0]
                                 |otherwise = f
-                                where f = e1 Value ++ e2 Value ++ [opCodes ! op]
+                                where f =  reverse (e2 Value  ++ e1 Value) ++ [opCodes ! op]
+                                      f2 = e1 Value ++ e2 Value ++ [opCodes ! op]
 
 
 opCodes :: Map String Instr
